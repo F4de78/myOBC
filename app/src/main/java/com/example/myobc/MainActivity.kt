d339baf53d5f12da9e00ed2bd164a23f0c2c418c
F4de78 <casazucca10@gmail.com>
2023-07-03 00:30:00 +0200

    started UI

---
+++ app/src/main/java/com/example/myobc/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myobc
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu , menu);
+        return true
+    }
+
+
+
+
+}
